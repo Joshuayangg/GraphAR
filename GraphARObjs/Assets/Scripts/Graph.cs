5fa596dd683c7372b0d7a1404f66f6df2fc417ac
joshuayang <josh.ym3@gmail.com>
2018-04-14 12:55:02 -0700

    Added some stuff used in CatLikeCoding's tutorial

---
+++ GraphARObjs/Assets/Scripts/Graph.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+//@Source: http://catlikecoding.com/unity/tutorials/basics/building-a-graph/
+
+public class Graph : MonoBehaviour {
+
+	public Transform pointPrefab;
+
+	void Awake () {
+		Transform point = Instantiate(pointPrefab);
+		point.localPosition = Vector3.right;
+
+		//		Transform point = Instantiate(pointPrefab);
+		point = Instantiate(pointPrefab);
+		point.localPosition = Vector3.right * 2f;
+	}
+}
