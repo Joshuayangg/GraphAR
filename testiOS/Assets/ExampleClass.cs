f4f0a5cd12b5850af1db0abad69cf07a62c143a6
ykimdavid <ykimdavid@gmail.com>
2018-04-14 18:00:16 -0700

    gui

---
+++ testiOS/Assets/ExampleClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleClass : MonoBehaviour {
+	public string function = "Enter Function";
+
+	void OnGUI() {
+		GUI.Label (new Rect (230, 150, 245, 40), "GraphAR");
+		function = GUI.TextField (new Rect (225, 200, 250, 25), function, 40);
+		if (GUI.Button (new Rect (300, 250, 100, 30), "Enter")) {
+			Debug.Log (function);
+		}
+	}
+}
